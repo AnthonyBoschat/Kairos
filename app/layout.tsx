c82db1fd3d922b656b7df99f94e1adfe13a69959
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-11-15 00:58:19 +0100

    Modification des variabels couleur et nettoyage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,20 @@
 import '@/styles/main.scss';
 import { Providers } from './providers';
 import s from "./styles.module.scss"
-import { Anuphan, Anton_SC } from 'next/font/google'
+import { Anuphan } from 'next/font/google'
 import withClass from '@/utils/class';
 
-const anuphan = Anuphan({
+const mulish = Anuphan({
   subsets: ['latin'],
   weight: ['400', '700'],
-  variable: '--font-anuphan',
-});
-const anton = Anton_SC({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-antonsc',
+  variable: '--font-mulish',
 });
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
 
   return (
     <html lang="fr">
-      <body className={withClass(s.layout, anuphan.variable, anton.variable)} suppressHydrationWarning>
+      <body className={withClass(s.layout, mulish.variable)} suppressHydrationWarning>
         <Providers>
           {children}
         </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
 import s from "./styles.module.scss"
-import AppTitle from "./_components/Title";
-import AppStatus from "./_components/Status";
-import { initStatuses } from "./actions/status";
-import { prisma } from "@/lib/prisma";
-import AppAuth from "./_components/Auth";
-
 
 export default async function Home() {
 
 
-  await initStatuses();
-  const statuses = await prisma.status.findMany();
 
   return (
     <div className={s.page}>
-      <div className={s.container}>
-        <div className={s.virgin}>
-          <AppTitle/>
-          <AppStatus statuses={statuses}/>
-        </div>
-        <div className={s.auth}>
-          <AppAuth/>
-        </div>
-      </div>
+      qzdqzd
     </div>
   );
 }
