dc2337dbfba3c4a17a45e62956c6c3c3005d1222
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-11-16 14:54:10 +0100

    Hover sur les dossiers

--- app/dashboard/_components/folderButton/index.tsx
+++ app/dashboard/_components/folderButton/index.tsx
@@ -3,6 +3,9 @@ import FolderSolidIcon from "@/components/ui/icons/FolderSolid"
 import s from "./styles.module.scss"
 import { Folder } from "@/types/folder"
 import FOLDER_COLORS from "@/constants/folderColor"
+import { useState } from "react"
+import ArrowLeftIcon from "@/components/ui/icons/ArrowLeft"
+import withClass from "@/utils/class"
 
 interface FolderProps{
     folder: Folder
@@ -12,12 +15,17 @@ interface FolderProps{
 export default function FolderButton({folder}:FolderProps){
 
 
+    const [isHover, setIsHover] = useState(false)
+
     return(
-        <button title="Accéder au contenu d'un dossier" className={s.container}>
+        <button onMouseLeave={() => setIsHover(false)} onMouseEnter={() => setIsHover(true)} title="Accéder au contenu d'un dossier" className={s.container}>
             <FolderSolidIcon color={folder.customColor ? folder.customColor : FOLDER_COLORS[folder.defaultColor ?? 0]} size={18} />
             <span>
                 {folder.title}
             </span>
+            <div className={withClass(s.indicator, isHover && s.active)}>
+                {isHover && <ArrowLeftIcon size={16}/>}
+            </div>
         </button>
     )
 }
