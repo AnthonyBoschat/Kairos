f610bd55affdff87e93bf98d1738cc72f831eb2d
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-12-07 23:57:30 +0100

    Indicateur au survol d'une tâche

--- app/dashboard/_components/content/_components/TaskItem/index.tsx
+++ app/dashboard/_components/content/_components/TaskItem/index.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { Task } from "@prisma/client"
 import s from "./styles.module.scss"
+import { useState } from "react"
+import withClass from "@/utils/class"
+import ArrowLeftIcon from "@/components/ui/icons/ArrowLeft"
 
 interface TaskItemProps{
     listColor:string,
@@ -10,11 +13,15 @@ interface TaskItemProps{
 
 export default function TaskItem(props:TaskItemProps){
 
+    const [isHover, setIsHover] = useState(false)
 
     return(
-        <li style={{backgroundColor:props.listColor}} className={s.container} key={props.task.id}>
+        <li onMouseLeave={() => setIsHover(false)} onMouseEnter={() => setIsHover(true)} style={{backgroundColor:props.listColor}} className={s.container} key={props.task.id}>
             <div className={s.content}>
                 {props.task.title}
+            </div>
+            <div className={withClass(s.indicator, (isHover) && s.active)}>
+                {isHover && <ArrowLeftIcon size={16}/>}
             </div>
         </li>
     )
