d54138ea64a2b5b74c7631187a3c579e234e5a4d
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-12-13 21:45:30 +0100

    Possibilité de supprimer une tâche

--- app/dashboard/_components/content/_components/TaskItem/index.tsx
+++ app/dashboard/_components/content/_components/TaskItem/index.tsx
@@ -4,10 +4,12 @@ import s from "./styles.module.scss"
 import { useState } from "react"
 import StarIcon from "@/components/ui/icons/Star"
 import handleResponse from "@/utils/handleResponse"
-import { toggleTaskFavorite } from "@/app/actions/task"
+import { deleteTask, toggleTaskFavorite } from "@/app/actions/task"
 import { useQueryClient } from "@tanstack/react-query"
 import { useAppSelector } from "@/store/hooks"
 import { toast } from "react-toastify"
+import withClass from "@/utils/class"
+import DeleteIcon from "@/components/ui/icons/delete"
 
 interface TaskItemProps{
     listColor:string
@@ -20,6 +22,7 @@ export default function TaskItem(props:TaskItemProps){
     const [isHover, setIsHover] = useState(false)
     const selectedFolderID = useAppSelector(store => store.folder.selectedFolderID)
     const queryClient = useQueryClient()
+    const isFavorite = props.task.favorite
 
     const handleAddTaskToFavorite = () => {
         handleResponse(async() => {
@@ -30,14 +33,28 @@ export default function TaskItem(props:TaskItemProps){
         })
     }
 
+    const handleDeleteTask = () => {
+        handleResponse(async() => {
+            const response = await deleteTask({taskID:props.task.id})
+            queryClient.invalidateQueries({queryKey: ["lists", selectedFolderID]})
+            toast.dismiss()
+            toast.success(response.message)
+        })
+    }
+
     return(
         <li onMouseLeave={() => setIsHover(false)} onMouseEnter={() => setIsHover(true)} style={{backgroundColor:props.listColor}} className={s.container}>
             <div className={s.content}>
-                <span>
+                <button className={withClass(s.favorite, isHover && s.visible, isFavorite && s.active)} onClick={handleAddTaskToFavorite}>
+                    <StarIcon animate active={props.task.favorite} size={18}/>
+                </button>
+
+                <span className={s.title}>
                     {props.task.title}
                 </span>
-                <button className={s.favorite} onClick={handleAddTaskToFavorite}>
-                    <StarIcon animate active={props.task.favorite} size={18}/>
+                
+                <button className={withClass(s.delete, isHover && s.visible)} onClick={handleDeleteTask}>
+                    <DeleteIcon size={20}/>
                 </button>
             </div>
         </li>
