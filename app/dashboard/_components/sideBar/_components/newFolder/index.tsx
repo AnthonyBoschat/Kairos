f8cde2d513f6082b46eddf51eeef9e36fdb95c50
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2025-12-17 23:44:01 +0100

    Si la touche entrer est presser pour ajouter un dossier / liste ou tâche, la fenêtre d'ajout reste ouverte pour des ajouts successif rapide.

--- app/dashboard/_components/sideBar/_components/newFolder/index.tsx
+++ app/dashboard/_components/sideBar/_components/newFolder/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import FolderSolidIcon from "@/components/ui/icons/FolderSolid"
 import s from "./styles.module.scss"
-import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from "react"
+import React, { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from "react"
 import { addFolder, getNextFolderColorIndexForCurrentUser } from "@/app/actions/folder"
 import FOLDER_COLORS from "@/constants/folderColor"
 import handleResponse from "@/utils/handleResponse"
@@ -23,21 +23,26 @@ export default function AddFolderButton(props:FolderProps){
 
     const isEnter = (e: React.KeyboardEvent) => e.key === "Enter";
     
-
     const fetchFolderColor = async() => {
         const color = await getNextFolderColorIndexForCurrentUser()
         setFolderColor(color)
     }
 
-    const handleAddFolder = useCallback(async() => {
+    const handleAddFolder = (title:string, keyboardEvent?:React.KeyboardEvent) => {
+        if(keyboardEvent) keyboardEvent.preventDefault()
         if(title.trim()){
             handleResponse(async () => {
                 const response = await addFolder({title:title})
                 toast.success(response.message)
             })
         }
-        props.setIsAddingFolder(false)
-    }, [title])
+        if(keyboardEvent){
+            setTitle("")
+            props.setIsAddingFolder(true)
+        }else{
+            props.setIsAddingFolder(false)
+        }
+    }
     
     useEffect(() => {
         fetchFolderColor()
@@ -46,14 +51,14 @@ export default function AddFolderButton(props:FolderProps){
     
     
     
-    useCallbackOnClickOutside(containerRef, handleAddFolder)
+    useCallbackOnClickOutside(containerRef, () => handleAddFolder(title))
 
     return(
         
         <button ref={containerRef} title="Création d'un nouveau dossier" className={s.container}>
             <FolderSolidIcon color={folderColor !== null ? FOLDER_COLORS[folderColor] : 'transparent'}  size={18} />
 
-            <input onKeyDown={(e) => isEnter(e) && handleAddFolder()} ref={titleRef} value={title} onChange={(e) => setTitle(e.currentTarget.value)} type="text"/>
+            <input onKeyDown={(e) => isEnter(e) && handleAddFolder(title, e)} ref={titleRef} value={title} onChange={(e) => setTitle(e.currentTarget.value)} type="text"/>
         </button>
     )
 }
